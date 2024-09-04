1fcdd0b4c378b318330d145c2affd94ecc2d3b9d
WIIXZII <155617336+WIIXZII@users.noreply.github.com>
2024-09-04 16:35:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
-const container = document.getElementById("container");
-const imageOne = document.querySelector(".image-1");
-const imageTwo = document.querySelector(".image-2");
-const btnYes = document.querySelector(".btn-yes");
-const btnNo = document.querySelector(".btn-no");
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
 
-function getRandomNumber(min, max) {
-  // Calculate the random number between min and max (inclusive)
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
 
-  return randomNumber;
-}
-
-btnNo.addEventListener("mouseover", (event) => {
-  const containerHeight = container.getBoundingClientRect().height;
-  const containerWidth = container.getBoundingClientRect().width;
-  const btnHeight = btnNo.getBoundingClientRect().height;
-  const btnWidth = btnNo.getBoundingClientRect().width;
-  const btnTop = btnNo.getBoundingClientRect().top;
-  const btnLeft = btnNo.getBoundingClientRect().left;
-
-  let newTop = btnTop;
-  let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-    newTop = getRandomNumber(0, containerHeight - btnHeight);
-  }
-
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-    newLeft = getRandomNumber(0, containerWidth - btnWidth);
-  }
-
-  btnNo.style.top = Math.floor(newTop) + "px";
-  btnNo.style.left = Math.floor(newLeft) + "px";
+yesBtn.addEventListener('click' , () => {
+    question.innerHTML = 'I Love You Too :)';
 });
 
-btnYes.addEventListener("click", (e) => {
-  btnNo.classList.add("hide");
-  imageOne.classList.add("hide");
-  imageTwo.classList.remove("hide");
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+    noBtn.style.leftt = i + 'px';
+    noBtn.style.top = j + 'px';
 });
